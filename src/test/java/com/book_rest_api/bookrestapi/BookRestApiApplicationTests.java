5f8e90a3230500ffd757d2c1798b0dce1d3bea29
alex-pominov <dev.alex.pominov@gmail.com>
2020-04-14 21:09:04 +0300

    init project with Spring initializr

---
+++ src/test/java/com/book_rest_api/bookrestapi/BookRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_rest_api.bookrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
