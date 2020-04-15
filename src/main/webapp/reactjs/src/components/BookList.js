6aa7f308b911175b04c18ed903f1b9bcf71b3619
alex-pominov <dev.alex.pominov@gmail.com>
2020-04-15 12:57:48 +0300

    added axios to Project and Impl fetch / display books

--- src/main/webapp/reactjs/src/components/BookList.js
+++ src/main/webapp/reactjs/src/components/BookList.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList } from "@fortawesome/free-solid-svg-icons";
-import { Card, Table } from "react-bootstrap";
+import { faList, faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
+import { Card, Table, Image, ButtonGroup, Button } from "react-bootstrap";
+import axios from "axios";
 
 const Booklist = () => {
+  const [books, setBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8080/rest/books");
+    setBooks(response.data);
+  }
+
   return (
     <Card className="border border-dark bg-dark text-white">
       <Card.Header>
@@ -22,9 +34,39 @@ const Booklist = () => {
             </tr>
           </thead>
           <tbody>
-            <tr align="center">
-              <td colSpan="6">No Books available</td>
-            </tr>
+            {books.length === 0 ? (
+              <tr align="center">
+                <td colSpan="6">No Books available</td>
+              </tr>
+            ) : (
+              books.map((book) => (
+                <tr key={book.id}>
+                  <td>
+                    <Image
+                      src={book.coverPhotoURL}
+                      roundedCircle
+                      width="25"
+                      height="25"
+                    />{" "}
+                    {book.title}
+                  </td>
+                  <td>{book.author}</td>
+                  <td>{book.isbnNumber}</td>
+                  <td>{book.price}</td>
+                  <td>{book.language}</td>
+                  <td align="center">
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary">
+                        <FontAwesomeIcon icon={faEdit} />
+                      </Button>{" "}
+                      <Button size="sm" variant="outline-danger">
+                        <FontAwesomeIcon icon={faTrash} />
+                      </Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </Table>
       </Card.Body>
